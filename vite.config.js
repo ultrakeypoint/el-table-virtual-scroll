dd812373f731195e7dd65e4474d90ed94d56bb22
ultrakeypoint <ultrakeypoint@gmail.com>
2024-07-09 22:40:52 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/el-table-virtual-scroll/'
+  // base: '/el-table-virtual-scroll/'
 })
