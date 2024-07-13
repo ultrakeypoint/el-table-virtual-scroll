e0ea8f2041bc4936c5c75eda754e62f8028c71c0
ultrakeypoint <ultrakeypoint@gmail.com>
2024-07-13 22:03:00 +0900

    update full height

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script>
 import VirtualScroll from "el-table-virtual-scroll";
-import bulkData from "@/assets/bulkData";
+import bulkData from "@/assets/js/bulkData";
 
 export default {
   name: "App",
   components: { VirtualScroll },
+  computed: {
+    height: function () {
+      return `${window.innerHeight - this.padding}px`;
+    },
+  },
   data() {
     return {
       list: bulkData,
       virtualList: [],
+      padding: 16,
     };
   },
 };
@@ -17,14 +23,14 @@ export default {
 <template>
   <virtual-scroll
     :data="list"
-    :item-size="62"
+    :item-size="32"
     key-prop="id"
     @change="(renderData) => (virtualList = renderData)"
   >
-    <el-table row-key="id" height="500px" :data="virtualList">
+    <el-table row-key="id" :height="height" :data="virtualList">
       <el-table-column prop="id" label="No" width="80"></el-table-column>
       <el-table-column prop="date" label="Date" width="100"></el-table-column>
-      <el-table-column prop="name" label="Name" width="100"></el-table-column>
+      <el-table-column prop="name" label="Name" width="60"></el-table-column>
       <el-table-column prop="address" label="Address"></el-table-column>
     </el-table>
   </virtual-scroll>
